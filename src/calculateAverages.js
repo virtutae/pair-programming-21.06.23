211fd7897eff853a5130fac15aaf4e0b7662586b
virtutae <virtutae@yahoo.it>
2023-06-21 09:53:51 +0100

    function signature

---
+++ src/calculateAverages.js
@@ -0,0 +1,41 @@
+/*
+### Task:
+
+Write a function which takes an array of numbers and returns the two separate averages of negative numbers and positive numbers found in the array.
+
+Return the averages as two elements of an array, or, if you know objects, as properties `averageOfPositives` and `averageOfNegatives`
+
+Use the value `null` to indicate when an average is not possible - e.g. when there are no positive numbers.
+
+Zero values are not positive or negative. You should exclude them from your calculations.
+
+### Example:
+
+`calcAveragesNegativeAndPositive([10, -100, 20, -200, -3])` 
+should return either the array
+
+ `[15, -101]`
+
+*or* the object
+
+`{ averageOfPositives: 15, averageOfNegatives: -101 }`
+
+where *`15`* is the average of all the positive numbers (10, 20)  and *`-101`* is the average of all the negative numbers (-100, -200, -3)
+*/
+
+/*
+PSEUDOCODE
+
+name: calculateAverages
+input: arrOfNums
+output: averages, an object with two numbers 
+
+*/
+
+
+
+
+function calculateAverages(){
+
+}
+
